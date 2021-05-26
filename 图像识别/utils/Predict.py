02c31ac8b63a7e6df4714d091efe2ea9d3d944e1
CQUEEN-lpy <79646025+CQUEEN-lpy@users.noreply.github.com>
2021-05-26 10:23:07 +0800

    enable exception catching

--- 图像识别/utils/Predict.py
+++ 图像识别/utils/Predict.py
@@ -2,112 +2,118 @@
 import========================================================================================================================================
 '''
 import os
-
 import pandas
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-import tensorflow as tf
-import pathlib
-import tensorflow.keras as keras
-import tensorflow.keras.layers as layers
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+from model.myEFF_predict import *
 from PIL import Image
 import numpy as np
 
 '''
-basic parameter config=======================================================================================================================================
+basic parameter config========================================================================================================================
 '''
 epochs = 100
-batch_size = 64
+batch_size = 256
 img_width = 244
 img_height = 244
 class_nums = 4
-checkpoint_path = '/home/zhang.xinxi/CV/checkpoint/mask_all/cp-0003.ckpt'
-data_path = pathlib.Path(r'/home/zhang.xinxi/CV/data/all')
-
-# load the data
-ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_path,
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+checkpoint_path = '/home/zhang.xinxi/CV/checkpoint/test/cp-0008.ckpt'
+filePath = '/home/zhang.xinxi/CV/data/all2/all/2'
+savePath = '/home/zhang.xinxi/CV/data/all2/csv/final'
+savePath += '/02.csv'
+class_names = ['V领', '圆领', '方领', '一字领']
 
 """
-build the model and load it from the check point =======================================================================================================================================
+build the model and load it from the check point =============================================================================================
 """
-# load the EFF_model
-EFF_model = tf.keras.applications.EfficientNetB6(
-    include_top=False, weights=None, input_tensor=None,
-    input_shape=(img_height, img_width, 3), pooling=None, classes=class_nums,
-    classifier_activation=None,
-)
-EFF_model.trainable = False
-
-
-# classifier
-classifier = keras.Sequential([
-    #layers.Dropout(0.5),
-    layers.Dense(class_nums, activation='softmax')]
-)
-
-# normalization
-norm_layer = keras.layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(img_height, img_width, 3))
-
-
-# create my own model and compile
-inputs = keras.Input(shape=(img_width, img_height, 3))
-x = inputs
-x = norm_layer(x)
-x = EFF_model(x, training=False)
-x = keras.layers.GlobalAveragePooling2D()(x)
-outputs = classifier(x)
-model = keras.Model(inputs, outputs)
-
-model.compile(optimizer=keras.optimizers.Adam(),
-              loss=keras.losses.CategoricalCrossentropy(label_smoothing=0.1),
-              metrics=['accuracy'])
-#model.load_weights(checkpoint_path)
+model = get_my_EFFmodel(img_height, img_width, class_nums, checkpoint_path)
 
 """
-predict====================================================================================================================
+predict======================================================================================================================================
 """
 result = []
-filePath = '/home/zhang.xinxi/CV/data/all/0'
+
 file_names = os.listdir(filePath)
-j = 1
-images = []
-paths = []
-final = None
-count = 0
+
+j = 1               # batch index
+images = []         # image column
+paths = []          # path column
+classes = []        # class column
+final = None        # the final pandas
+count = 0           # count the number of image
 for i in file_names:
     count += 1
-    if j <= batch_size:
-        paths.append(i)
-        img = np.array(Image.open(filePath + '/' +i))
-        img = tf.image.resize(img, (244, 244))
-        j += 1
-        images.append(img)
-    else:
-        print(count)
-        images = tf.convert_to_tensor(images, dtype='float32')
-        outputs = np.array(model(images))
-        index = np.array(tf.math.argmax(outputs, 1))
-        scores = []
-        for k in range(index.size):
-            scores.append(outputs[k][index[k]])
-        index = np.expand_dims(index, 1)
-        scores = np.expand_dims(np.array(scores), 1)
-        paths = np.expand_dims(np.array(paths), 1)
-        total = np.concatenate((paths, index), 1)
-        total = np.concatenate((total, scores), 1)
-        if final is not None:
-            final = np.concatenate((final, total), 0)
-        else:
-            final = total
+    j += 1
+
+    try:
+        if j > batch_size:
+            print(count)
+            images = tf.convert_to_tensor(images, dtype='float32')
+            outputs = np.array(model(images))
+            index = np.array(tf.math.argmax(outputs, 1))
+            scores = []
+            for k in range(index.size):
+                scores.append(outputs[k][index[k]])
+                classes.append(class_names[index[k]])
+            index = np.expand_dims(index, 1)
+            scores = np.expand_dims(np.array(scores), 1)
+            classes = np.expand_dims(np.array(classes), 1)
+            paths = np.expand_dims(np.array(paths), 1)
+            total = np.concatenate((paths, index), 1)
+            total = np.concatenate((total, scores), 1)
+            total = np.concatenate((total, classes), 1)
+            if final is not None:
+                final = np.concatenate((final, total), 0)
+            else:
+                final = total
+            paths = []
+            images = []
+            classes = []
+            j = 1
+    except BaseException as ex:
+        print(ex)
+        print('111111')
         paths = []
         images = []
+        classes = []
         j = 1
 
-save_dir = ''
+    try:
+        img = np.array(Image.open(filePath + '/' + i))
+        img = tf.image.resize(img, (244, 244))
+
+        if img.shape == (244, 244, 3):
+            paths.append(i)
+            images.append(img)
+    except BaseException as ex:
+        print(
+            '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+        print(ex)
+        print(i)
+        print(img)
+
+print(count)
+images = tf.convert_to_tensor(images, dtype='float32')
+outputs = np.array(model(images))
+index = np.array(tf.math.argmax(outputs, 1))
+scores = []
+for k in range(index.size):
+    scores.append(outputs[k][index[k]])
+    classes.append(class_names[index[k]])
+
+index = np.expand_dims(index, 1)
+scores = np.expand_dims(np.array(scores), 1)
+classes = np.expand_dims(np.array(classes), 1)
+paths = np.expand_dims(np.array(paths), 1)
+
+total = np.concatenate((paths, index), 1)
+total = np.concatenate((total, scores), 1)
+total = np.concatenate((total, classes), 1)
+if final is not None:
+    final = np.concatenate((final, total), 0)
+else:
+    final = total
+
 final = pandas.DataFrame(final)
-final.set_index(0)
-final.to_csv(save_dir)
+final.columns = ['文件名', '类别id', '得分', '类别']
+final = final.set_index('文件名')
+final.to_csv(savePath, encoding='utf_8_sig')
